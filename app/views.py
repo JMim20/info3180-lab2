71b42aa52835c01ee03d157b4a0e19127ea46246
Jamila McGowan <jamcarr280@gmail.com>
2023-02-12 19:39:43 -0800

    completed info3180-lab2. Added profile.html, updated app py with changes associate to the profile route.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+
 
 
 ###
@@ -45,3 +47,16 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+@app.route('/profile')
+def profile():
+    
+    return render_template('profile.html', date=format_date_joined())
+
+def format_date_joined():
+    now = datetime.datetime.now() # today's date
+    date_joined = datetime.date(2019, 2, 7) # a specific date
+    ## Format the date to return only month and year date
+    ##print ("Joined " + date_joined.strftime("%B, %Y"))
+    return( "Joined " + date_joined.strftime("%B, %Y"))
